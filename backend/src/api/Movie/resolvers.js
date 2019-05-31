4a9049e7b3d30987467dc6236e8bd7435c849981
yoohl <yooh@zuminternet.com>
2019-05-31 16:35:33 +0900

    movie db로 수정, add/del 추가

--- backend/src/api/Movie/resolvers.js
+++ backend/src/api/Movie/resolvers.js
@@ -1,12 +1,15 @@
-import { people, getById } from "../../movieDb"
-
+import { getMovies, getById, addMovie, deleteMovie } from "../../movieDb"
 
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => {
+    movies: () => getMovies(),
+    movie: (_, { id }) => {
       return getById(id) // 화살표 함수에서 중괄호를 제거 하면 암묵적으로 return 됨, 'person: (_, { id }) => getById(id) 와 같음'
     }
+  },
+  Mutation: {
+    addMovie: (_, {name, score}) => addMovie(name, score),
+    deleteMovie: (_, {id}) => deleteMovie(id)
   }
 };
 export default resolvers;
--- backend/src/movieDb.js
+++ backend/src/movieDb.js
@@ -1,26 +1,44 @@
-export const people = [
+let movies = [
   {
-    id: "1",
-    name: 'yu hyelim',
-    age: 10,
-    gender: "female"
+    id: 0,
+    name: '기생충',
+    score: 3,
   },
   {
-    id: "2",
-    name: 'df',
-    age: 11,
-    gender: "female"
+    id: 1,
+    name: 'Avengers - The new one',
+    score: 8,
   },
   {
-    id: "3",
-    name: 'SDFGERG',
-    age: 21,
-    gender: "male"
+    id: 2,
+    name: '알라딘',
+    score: 4,
   }
 ]
 
+export const getMovies = () => movies;
 
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0]
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0]
+}
+
+export const deleteMovie = id => {
+  const cleanMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanMovies.length) {
+    movies = cleanMovies;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: parseInt(`${movies.length + 1}`),
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 }
